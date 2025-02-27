8e04fc50d76fbb13fd3a9052fb426b023505818a
Victoriano Izquierdo <victoriano@gmail.com>
2025-02-27 20:45:13 +0100

    now npm run dev and npm run preview works both

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     assetsDir: 'assets',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
+        main: resolve(__dirname, 'index.html'),
       },
     },
   },
